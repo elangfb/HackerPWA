d411daf5ae07401c5155bc89ad289edd39d5a937
elangfb <lngfjr@gmail.com>
2024-05-11 23:06:17 +0700

    Update Dropdown & Fix CSS

--- components/cards/CardProduct.vue
+++ components/cards/CardProduct.vue
@@ -12,20 +12,20 @@ const props = defineProps({
 
 <template>
     <section class="bg-white shadow-xl rounded-xl overflow-hidden">
-        <div :class="'w-full h-[200px] p-5 bg-gray-300'">
-            <img :src="props.products.image" class="w-full h-full object-contain"/>   
-        </div>
-        <div class="px-5 pb-5 pt-9 relative">
-            <NuxtLink :to="`/products/${props.products.id}`">
-            <h3 class="text-lg font-bold mb-4 text-limit limit-2"> {{ props.products.name }}</h3>
+      <div :class="`w-full h-[200px] p-5 bg-gray-300`">
+        <img :src="props.products.image" class="w-full h-full object-contain"/>
+      </div>
+      <div class="px-5 pb-5 pt-9 relative">
+        <NuxtLink :to="`/products/${props.products.id}`">
+          <h3 class="text-lg font-bold mb-4 text-limit limit-2">{{ props.products.name }}</h3>
         </NuxtLink>
+        <div class="flex justify-evenly items-center">
+          <span class="text-sm font-normal">{{ props.products.category }}</span>
+          <span class="text-sm font-normal">${{ props.products.price }}</span>
         </div>
-        <div class="flex justify-between items-center">
-            <span class="text-sm font-normal">{{ props.products.category }}</span>
-            <span class="text-sm font-normal">${{ props.products.price }}</span>
+        <div :class="`cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px] bg-white shadow-xl rounded-full flex justify-center items-center hover:bg-blue-600 hover:text-white transition duration-300`">
+          <i class="ri-shopping-cart-2-line"></i>
         </div>
-        <div :class="'cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px] bg-white shadow-xl rounded-full flex justify-center items-center hover:bg-blue-600 hover:text-white transition duration-300'">
-        <i class="ri-shopping-cart-2-line"></i>
-        </div>
+      </div>
     </section>
-</template>
+  </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: "stylesheet", href:
-          "https://cdn.jsdelivr.net/npm/remixicon@e.5.0/fonts/remixicong.css"
+        { rel: "stylesheet", href:"https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
         }
       ],
     }
